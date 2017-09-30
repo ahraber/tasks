e0f23b29379af36bfc7116e8464b034485011fe8
Alex <alexhraber@gmail.com>
2017-09-30 01:34:20 -0700

    noop if clean diff

--- scripts/tasks-backup.sh
+++ scripts/tasks-backup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 DATE=`date +%Y-%m-%d-%H`
+PREV=`ls -Art ~/tasks/old/|grep bak| tail -n 1`
 
-cp ~/tasks/tasks ~/tasks/old/tasks-`echo $DATE`.bak
+diff ~/tasks/tasks ~/tasks/old/`echo $PREV`
+
+if [ $? -eq 0 ]; then
+    exit 0 #noop
+else
+    cp ~/tasks/tasks ~/tasks/old/tasks-`echo $DATE`.bak
+fi
