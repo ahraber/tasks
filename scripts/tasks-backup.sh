d63cdcf30a18fb491b21e6e740c1d5ae709a2211
Alex <alexhraber@gmail.com>
2017-09-30 01:36:20 -0700

    reverting to pre-noop

--- scripts/tasks-backup.sh
+++ scripts/tasks-backup.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
 DATE=`date +%Y-%m-%d-%H`
-PREV=`ls -Art ~/tasks/old/|grep bak| tail -n 1`
 
-diff ~/tasks/tasks ~/tasks/old/`echo $PREV`
-
-if [ $? -eq 0 ]; then
-    exit 0 #noop
-else
-    cp ~/tasks/tasks ~/tasks/old/tasks-`echo $DATE`.bak
-fi
+cp ~/tasks/tasks ~/tasks/old/tasks-`echo $DATE`.bak
